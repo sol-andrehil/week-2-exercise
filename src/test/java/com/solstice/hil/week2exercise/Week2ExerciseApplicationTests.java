25cbb2804e83c43b8bb9b0d4660fe94f8b42349b
Andre Hildinger <ahildinger@solstice.com>
2018-07-10 15:39:54 -0500

    First commit

---
+++ src/test/java/com/solstice/hil/week2exercise/Week2ExerciseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.solstice.hil.week2exercise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Week2ExerciseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
